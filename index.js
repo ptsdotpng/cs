3b0ce32552b78ece79a2b63ac6d8baccbebaa96e
( ._.) <pts@pop-os.localdomain>
2021-01-08 21:36:09 -0500

    eod

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
-console.log("cs V0.0.0")
+
+/*
+ * main
+ * right now  everything
+ * should move on to be the entry point
+ * of the game
+ *
+ */
+const main = async function main() {
+	/*
+	 * game loop
+	 * - init
+	 * - get targets - for updates
+	 * - start the game loop
+	 *   - run every 1s
+	 *   - add to the state and update view
+	 */
+
+	let state = {
+		code: {
+			name: "code",
+			rate: 1,
+			total: 1,
+			current: 1
+		}
+	}
+
+	let codeElem = document.getElementById(`code-techtree`);
+	let gen = document.getElementById(`code-techtree-generating`)
+
+	const tick = function tick() {
+		state.code.total += 1;
+		gen.innerHTML = state.code.total
+
+		setTimeout(tick, 1000);
+	}
+
+
+
+	tick();
+
+}
+
+
+
+main();
+
+
+
+
+
